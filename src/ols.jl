d65fb4d49188daae8710a0ba94336e1d5eb69fbb
Phillip Alday <me@phillipalday.com>
2021-02-17 12:08:29 +0100

    pooled olsranef

--- src/ols.jl
+++ src/ols.jl
@@ -1,25 +1,42 @@
 """
-    olsranef(model::LinearMixedModel)
+    olsranef(model::LinearMixedModel, method=:simultaneous)
 
 Compute the group-level estimates using ordinary least squares.
 
-This is somewhat similar to the conditional modes / BLUPs computed without shrinkage.
+This is somewhat similar to the conditional modes / BLUPs computed without
+shrinkage.
 
-Optionally, instead of using the shrunken random effects from `ranef`, within-group OLS
-estimates can be computed and used instead with [`olsranef`](@ref). There is no shrinkage
-of the group-level estimates with this approach, which means singular estimates can be
-avoided. However,
+There is no shrinkage of the group-level estimates with this approach, which
+means singular estimates can be avoided. However, this also means the random
+effects design matrix must not be singular.
+
+Two methods are provided:
+1. (default) OLS estimates computed for all strata (blocking variables)
+   simultaneously with `method=simultaneous`. This pools the variance
+   across estimates but does not shrink the estimates.
+2. OLS estimates computed within each stratum with `method=stratum`. This
+   method is equivalent for example to computing each subject-level and each
+   item-level regression separately.
+
+For fully balanced designs with a single blocking variable, these methods will
+give the same results.
 
 !!! warning
-    If the design matrix for the random effects is rank deficient (e.g., through the use of
-    `MixedModels.fulldummy` or missing cells in the data), then this method will fail.
+    If the design matrix for the random effects is rank deficient (e.g., through
+    the use of `MixedModels.fulldummy` or missing cells in the data), then this
+    method will fail.
 """
-function olsranef(model::LinearMixedModel{T}) where {T}
+function olsranef(model::LinearMixedModel{T}, method=:simultaneous) where {T}
     fixef_res = copy(response(model))
     # what's not explained by the fixed effects has to be explained by the RE
     X = model.X
     β = model.β # (X'X) \ (X'fixef_res)
     mul!(fixef_res, X, β, one(T), -one(T))
+
+    return olsranef(model, fixef_res, Val(method))
+end
+
+function olsranef(model, fixef_res, ::Val{:stratum})
 
     blups = Vector{Matrix{Float64}}()
     for trm in model.reterms
@@ -42,6 +59,26 @@ function olsranef(model::LinearMixedModel{T}) where {T}
         push!(blups, hcat(re...))
     end
 
+    return blups
+end
+
+
+function olsranef(model, fixef_res, ::Val{:simultaneous})
+    X = hcat(model.reterms...)
+    flatblups = X'X \ X'fixef_res
+
+    blups = Vector{Matrix{Float64}}()
+
+    offset = 1
+    for trm in model.reterms
+        chunksize = size(trm, 2)
+        ngrps = length(trm.levels)
+        npreds = length(trm.cnames)
+        re = Matrix(reshape(view(flatblups, offset:(offset+chunksize-1)),
+                               npreds, ngrps))
+        offset += chunksize
+        push!(blups, re)
+    end
     return blups
 end
 
